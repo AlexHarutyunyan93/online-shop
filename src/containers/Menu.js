c05b965e0a0241d7fc01ba77308f643106cf407b
AlexHary <47464816+AlexHary@users.noreply.github.com>
2019-09-13 00:05:24 +0400

    Update Menu.js
    Upgrading container...

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,14 +1,9 @@
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as cartActions from '../actions/cart';
 import TopHeader from '../components/Header/Top/Top';
 
 const mapStarteToProps = ({ cart }) => ({
     isReady: cart.isReady,
     count: cart.items.length
 });
-const mapDispatchToProps = dispatch => ({
-    ...bindActionCreators(cartActions,dispatch)
-});
 
-export default connect(mapStarteToProps, mapDispatchToProps)(TopHeader);
+export default connect(mapStarteToProps)(TopHeader);
