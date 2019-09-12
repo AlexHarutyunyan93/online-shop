366bf7803fce6837427047072c06d485e23ac017
AlexHary <47464816+AlexHary@users.noreply.github.com>
2019-09-12 23:57:32 +0400

    Update Main.jsx
    Upgrading component...

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,19 +5,20 @@ import Home from './../../containers/Home';
 import Cart from './../../containers/Cart';
 // import Product from '../../containers/Product';
 import Collection from "../../containers/Collections";
-import request from '../../actions/http';
+import About from './About/About';
 
 const Main = () => {
     return (
         <main className="main-content-wrapper">
             <div className="main-content">
-               <Route exact path='/' render={() => <Home url='/api/spring-summer' request={ request } /> } />
-               <Route path='/cart' render={() => <Cart /> } />
-               {/*<Route path='/products' render={() => <Product /> } />*/}
-               <Route path='/Spring-Summer' render={() => <Collection url='/api/spring-summer' request={ request } /> } />
-               <Route path='/Winter-Oen' render={() => <Collection url='/collections/WinterOen.json' request={ request } /> } />
-               <Route path='/Evening-Dresses' render={() => <Collection url='/collections/EveningDresses.json' request={ request } /> } />
-               <Route path='/Kids' render={() => <Collection url='/collections/Kids.json' request={ request } /> } />
+               <Route exact path='/' render={() => <Home url='/data.json' /> } />
+                <Route path='/cart' render={() => <Cart /> } />
+                <Route path='/about' render={() => <About /> } />
+               {/*/!*<Route path='/products' render={() => <Product /> } />*!/*/}
+               <Route path='/Spring-Summer' render={() => <Collection url='/api/spring-summer' /> } />
+               <Route path='/Winter-Oen' render={() => <Collection url='/collections/WinterOen.json' /> } />
+               <Route path='/Evening-Dresses' render={() => <Collection url='/collections/EveningDresses.json' /> } />
+               <Route path='/Kids' render={() => <Collection url='/collections/Kids.json' /> } />
             </div>
         </main>
     );
@@ -25,3 +26,4 @@ const Main = () => {
 };
 
 export default Main;
+
