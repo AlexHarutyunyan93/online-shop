dfd1fd03f8204bbff26b49b9741ada7e96c9c415
AlexHary <47464816+AlexHary@users.noreply.github.com>
2019-09-13 00:00:10 +0400

    Update Home.jsx
    Upgrading component...

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -1,41 +1,33 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import './home.css';
 import SpecialCategories from "../SpecialCategories/SpecialCategories";
 import PromoSlider from "../PromoSlider/PromoSlider";
 const NewCollections = lazy( () => import("../NewCollections/NewCollections"));
 const SellOut = lazy(() => import("../SellOut/SellOut"));
-const About = lazy(() => import("../About/About"));
+const About = lazy(() => import("./About/About"));
 
 
-class Home extends Component {
-    state = {
-        dresses: null
-    };
-    componentDidMount() {
-        const { url, request } = this.props;
-        request(url, res => this.setState({dresses: res}) )
-    }
-    render() {
-        const { dresses } = this.state;
-        return(
-            <div className="home-wrapper">
-                {
-                    dresses === null ? "...Loading"
-                        :
-                        <div className="home">
+function Home(props) {
 
-                            <SpecialCategories/>
-                            <Suspense fallback={<div>....Loading</div>}>
-                                <PromoSlider/>
-                                <NewCollections dresses={dresses}/>
-                                <SellOut dresses={dresses}/>
-                                <About />
-                            </Suspense>
-                        </div>
-                }
-            </div>
-        )
-    }
+const dresses = props.dresses;
+    return(
+        <div className="home-wrapper">
+            {
+                dresses === null ? "...Loading"
+                    :
+                    <div className="home">
+                        <SpecialCategories/>
+                        <Suspense fallback={<div>....Loading</div>}>
+                            <PromoSlider/>
+                            <NewCollections dresses={dresses}/>
+                            <SellOut dresses={dresses}/>
+                            <About />
+                        </Suspense>
+                    </div>
+            }
+        </div>
+    )
 }
+
 
 export default Home;
