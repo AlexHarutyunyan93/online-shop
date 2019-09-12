7f0cfcf0efc0d8ff6264e7009bec690854b24076
AlexHary <47464816+AlexHary@users.noreply.github.com>
2019-09-13 00:04:33 +0400

    Update Home.js
    Upgrading container...

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import Home from '../components/Main/Home/Home';
 import { bindActionCreators } from 'redux';
 import * as dressesActions from '../actions/dresses';
+import request from '../actions/http';
 
+class HomePage extends React.Component {
+    state = {
+        dresses: null
+    };
 
+    componentDidMount() {
+        const {url} = this.props;
+        request(url, res => this.setState({dresses: res}))
+    }
+    render() {
+        return (
+            <Home dresses={ this.state.dresses } />
+        )
+    }
+}
 
 const mapStateToProps = ({ dresses }) => ({
     dresses: dresses.items,
@@ -17,4 +33,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
     mapStateToProps,
     mapDispatchToProps,
-)(Home);
+)(HomePage);
