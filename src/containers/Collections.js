f162459cacd0dedebfaa0e00fa955550be1952c4
AlexHary <47464816+AlexHary@users.noreply.github.com>
2019-09-13 00:03:45 +0400

    Update Collections.js
    Upgrading container....

--- src/containers/Collections.js
+++ src/containers/Collections.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import Collection from '../components/Main/Collections/Collection';
 import { bindActionCreators } from 'redux';
 import * as dressesActions from '../actions/dresses';
+import request from '../actions/http';
 
+class Collections extends React.Component {
+    componentDidMount() {
+        const { setDresses, url } = this.props;
+        request(url, setDresses);
+    }
+    render(){
+        return(
+            <Collection {...this.props} />
+        )
+    }
+};
 
 const mapStateToProps = ({ dresses }) => ({
     dresses: dresses.items,
@@ -16,4 +29,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
     mapStateToProps,
     mapDispatchToProps,
-)(Collection);
+)(Collections);
