62484d928bdb9c6fadbbb55a48dda1fa3155ba12
AlexHary <47464816+AlexHary@users.noreply.github.com>
2019-09-13 00:01:30 +0400

    Update SpecialCategories.jsx
    Upgrading component...

--- src/components/Main/SpecialCategories/SpecialCategories.jsx
+++ src/components/Main/SpecialCategories/SpecialCategories.jsx
@@ -9,7 +9,7 @@ const SpecialCategories = () => {
                 <div className="special-category">
                     <NavLink to='/spring-summer' className="category-inner" href="/">
                         <div className="category-image-inner">
-                            <img className="category-image" src="https://static-eu.insales.ru/images/products/1/765/143393533/large_image1xxl__4_.jpg" alt=""/>
+                            <img className="category-image" src="http://localhost:4000/uploads/home/special-category-1.jpg" alt=""/>
                             <div className="category-caption">
                                 Весна-Лето
                             </div>
@@ -19,7 +19,7 @@ const SpecialCategories = () => {
                 <div className="special-category">
                     <NavLink to='/winter-oen' className="category-inner" href="/">
                         <div className="category-image-inner">
-                            <img className="category-image" src="https://static-eu.insales.ru/images/products/1/663/143393431/large_image1xxl__2_.jpg" alt=""/>
+                            <img className="category-image" src="http://localhost:4000/uploads/home/special-category-2.jpg" alt=""/>
                             <div className="category-caption">
                                 Осень-Зима
                             </div>
@@ -29,7 +29,7 @@ const SpecialCategories = () => {
                 <div className="special-category">
                     <NavLink to='/evening-dresses' className="category-inner" href="/">
                         <div className="category-image-inner">
-                            <img className="category-image" src="https://static-eu.insales.ru/images/products/1/765/143393533/large_image1xxl__4_.jpg" alt=""/>
+                            <img className="category-image" src="http://localhost:4000/uploads/home/special-category-3.jpg" alt=""/>
                         <div className="category-caption">
                             Вечерние платья
                         </div>
@@ -39,7 +39,7 @@ const SpecialCategories = () => {
                 <div className="special-category">
                     <NavLink to='/kids' className="category-inner" href="/">
                         <div className="category-image-inner">
-                            <img className="category-image" src="https://static-eu.insales.ru/images/products/1/618/143393386/large_image3xxl.jpg" alt=""/>
+                            <img className="category-image" src="http://localhost:4000/uploads/home/special-category-4.jpg" alt=""/>
                             <div className="category-caption">
                                 Детские
                             </div>
