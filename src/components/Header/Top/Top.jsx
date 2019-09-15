359a2123ac8333bb1c8b934b5e85cd0318d87e86
AlexHarutyunyan93 <47464816+AlexHarutyunyan93@users.noreply.github.com>
2019-09-16 00:41:39 +0400

    Update Top.jsx
    Some changes to choose language for page...

--- src/components/Header/Top/Top.jsx
+++ src/components/Header/Top/Top.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import './top.css';
 
-const Top = ({count}) => {
+const Top = ({count, language, setLanguage}) => {
     return(
         <nav className='top-header-wrapper'>
             <div className='top-header'>
                 <ul className='top-header-nav-bar'>
                     <li>Каталог</li>
-                    <li>О компании</li>
+                    <li>
+                        <NavLink to="/about">
+                            О компании
+                        </NavLink>
+                    </li>
                     <li>Контакты</li>
                 </ul>
                 <div className="top-header-right-sidebar">
@@ -16,6 +20,13 @@ const Top = ({count}) => {
                         <div className="header-cart">Корзина</div>
                         <div className="header-cart-count">{ count }</div>
                     </NavLink>
+                </div>
+                <div className="language">
+                    <select onChange={(e) => setLanguage(e.target.value)} value={language}>
+                        <option value="0">RU</option>
+                        <option value="1">EN</option>
+                        <option value="2">AM</option>
+                    </select>
                 </div>
             </div>
         </nav>
