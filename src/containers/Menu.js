2646f801719023bccc020063b48b2e985df217f4
AlexHarutyunyan93 <47464816+AlexHarutyunyan93@users.noreply.github.com>
2019-09-16 00:47:04 +0400

    Update Menu.js
    I added an action that will change the state of the page, depending on the chosen language.

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 import TopHeader from '../components/Header/Top/Top';
+import {bindActionCreators} from "redux";
+import * as languageSettings from '../actions/language';
 
-const mapStarteToProps = ({ cart }) => ({
-    isReady: cart.isReady,
+const mapStarteToProps = ({ cart, language }) => ({
+    language: language.language,
     count: cart.items.length
 });
+const mapDispatchToProps = dispatch => ({
+    ...bindActionCreators(languageSettings, dispatch),
+});
 
-export default connect(mapStarteToProps)(TopHeader);
+export default connect(mapStarteToProps, mapDispatchToProps)(TopHeader);
