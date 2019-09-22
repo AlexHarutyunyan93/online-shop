096c35cae5f4818318446f07d0e789cf6dbec3b4
AlexHarutyunyan93 <47464816+AlexHarutyunyan93@users.noreply.github.com>
2019-09-22 11:41:49 +0400

    Update Product.jsx
    Update product...

--- src/components/Main/Product/Product.jsx
+++ src/components/Main/Product/Product.jsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './product.css';
+import axios from "axios";
 
 const Product = (props) => {
-    const { pictureURL, price, title, addToCart, discount, key } = props;
+    const lan = {
+        price: ["руб"]
+    };
+    const [data, setData] = useState({});
+    const url = `http://localhost:4000/api${window.location.pathname}/?language=0`;
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(url);
+            setData(result.data);
+        };
+        fetchData();
+    }, []);
+    const {title, feutures, imageUrls, description, colors, price} = data;
+    const {addToCart} = props;
     return (
-        <div className='product-container'>
-            <div className='product-control'>
-                <div className="product-img">
-                    <img src={ pictureURL } alt={ title } />
+        !data.title ? "loading" :
+            <div className='product-container-wrapper'>
+                <div className="product-container">
+                    <div className="product-img-wrapper">
+                        <div className="product-img-list">
+                            {
+                                imageUrls.map(e => <img src={e} alt={title} />)
+                            }
+                        </div>
+                        <div className="show-one">
+                            <img src={imageUrls[0]} alt={title} />
+                        </div>
+                    </div>
+                    <div className="descriptions">
+                        <h3 className="product-title">{title}</h3>
+                        <div className="price">{price} {lan.price[0]}</div>
+                        <button >Add to cart</button>
+                        <div className="feutures">
+                            <ul>
+                                {
+                                    feutures.map((e, index) => <li key={index}>{ e }</li>)
+                                }
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className="control-bar">
-sdasd
+                <div className="product-desc">
+                    <h4>Description</h4>
+                    <p>{description[0]}</p>
                 </div>
             </div>
-            <div className="product-description">
-
-            </div>
-        </div>
     )
 };
+
 
 export default Product;
